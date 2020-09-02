d6c9c578fd1e7d4c12cfae95e2400044636459a3
KangJunewoo <brianjune.dev@gmail.com>
2020-09-03 02:30:32 +0900

    [2일차] 말도많고 탈도많던 user부분 구현 끝

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
+
 
 # Register your models here.
+# 튜플 구성 주의할것! 쉽표 마지막에 안달아주면 그냥 string으로 인식함.
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Banana",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
+
+
+# list_display = ('username', 'email', 'gender', 'language', 'currency', 'superhost')
+# list_filter = ('language', 'currency', 'superhost')
+
+
+# 데코레이터 쓴 건 다음과 같다.
+# admin.site.register(models.User, CustomUserAdmin)
