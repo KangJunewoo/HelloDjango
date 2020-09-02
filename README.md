1eb10ad93e92514928140c4a1bcf0445cb8858cb
KangJunewoo <brianjune.dev@gmail.com>
2020-09-03 02:51:39 +0900

    [2일차] reacap & 데이터 초기화

--- README.md
+++ README.md
@@ -31,7 +31,27 @@ linter는 flake8 formatter는 black을 쓰고 있구나.
 처음 써봐서 좋은 지는 잘 모르겠다.  
 또한 장고 공홈을 보는 습관을 들이자.  
 
-## 에러고치면서 배우기
+### customizing user
+- config는 마스터폴더같은거.
+- 나머지는 application
+  - application은 function들의 group
+  - users에서 models와 admin을 커스터마이징할 수 있다.
+- model은 fields로 이루어짐.
+  - 다양한 종류의 필드들.
+  - 유효성검사까지 장고가 알아서 다해줌.
+  - 필드를 정의한 파이썬 코드는 django orm을 통해 sql로 만들어져 db에 들어감.
+- 모델을 직접 보려면 admin 패널에서 가능했지.
+  - admin.py에서 모델을 등록하려.면 register를 했어야 했음. 데코레이터 사용했던거!
+  - admin 패널의 배치를 바꾸는 것도 파이썬을 만지면 됨. fieldsets 같은거!(그 파란색 소제목)
+    - list_display도 있었고.
+    - list_filter도 있었고
+- AbstractUser 상속하는 부분은 일단 외우자.
+- /config/settings에 등록해주기
+  - users.apps.UsersConfig 등록했었지.
+  - AUTH_USER_MODEL = "users.User" 넣어서 우리가 만든 User를 쓴다고 알려줬었어.
+  - 일반 장고 유저모델도 대부분의 경우엔 충-분히 쓸만하다.
+  - 
+## tips
 ### user모델 변경하려고 할 때 migration 순서 오류
 최대한 순수한 상태로 돌아가
 ```
@@ -41,3 +61,8 @@ python manage.py migrate contenttypes zero
 python manage.py migrate sessions zero
 ```
 로 싹 초기화해준 담에 진행.
+
+### 데이터 비우는 방법
+- db, pycache, migrations 모두 삭제한 뒤
+- makemigrations, migrate 적용.
+- migration은 최대한 적게 해주는게 좋다.
--- users/models.py
+++ users/models.py
@@ -12,30 +12,23 @@ class User(AbstractUser):
     GENDER_CHOICES = (
         (GENDER_MALE, "Male"),
         (GENDER_FEMALE, "Female"),
-        (GENDER_OTHER, "Other")
+        (GENDER_OTHER, "Other"),
     )
 
     LANGUAGE_ENGLISH = "en"
     LANGUAGE_KOREAN = "ko"
 
-    LANGUAGE_CHOICES = (
-        (LANGUAGE_ENGLISH, "English"),
-        (LANGUAGE_KOREAN, "Korean")
-    )
+    LANGUAGE_CHOICES = ((LANGUAGE_ENGLISH, "English"), (LANGUAGE_KOREAN, "Korean"))
 
     CURRENCY_USD = "usd"
     CURRENCY_KRW = "krw"
 
     CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
 
-    avatar = models.ImageField(null=True, blank=True)
-    gender = models.CharField(
-        choices=GENDER_CHOICES, max_length=10, null=True, blank=True
-    )
-    bio = models.TextField(default="", null=True, blank=True)
-    birthdate = models.DateField(null=True)
+    avatar = models.ImageField(blank=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
+    bio = models.TextField(blank=True)
+    birthdate = models.DateField(blank=True, null=True)
     language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, blank=True)
-    currency = models.CharField(
-        choices=CURRENCY_CHOICES, max_length=3, null=True, blank=True
-    )
+    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True)
     superhost = models.BooleanField(default=False)
