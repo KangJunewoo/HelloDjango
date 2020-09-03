177e279c88694f1c5ced68b87a9cf9ffc8618048
KangJunewoo <brianjune.dev@gmail.com>
2020-09-04 03:25:29 +0900

    [3&4일차] 오류 해결. 데이터 그때그때 잘 만들자.

--- rooms/admin.py
+++ rooms/admin.py
@@ -5,7 +5,7 @@ from . import models
 # Register your models here.
 
 
-@admin.register(models.RoomType)
+@admin.register(models.RoomType, models.Amenity, models.Facility, models.HouseRule)
 class ItemAdmin(admin.ModelAdmin):
     pass
 
